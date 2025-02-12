164e4a058594945f3dcd661b22f1a95b1b25e8c5
Luis Felipe <luisfelipearaujo503@gmail.com>
2025-02-12 12:05:21 -0300

    Tentanod usar KAN

--- main_KAN.py
+++ main_KAN.py
@@ -13,7 +13,7 @@ import gc
 import shutil
 from sklearn.model_selection import GroupKFold, KFold
 from sklearn.model_selection import train_test_split
-from pykan import KAN
+from kan import KAN
 
 
 torch.backends.cuda.matmul.allow_tf32 = True
@@ -27,61 +27,15 @@ torch.manual_seed(seed_pesos)
 # com regressão na KAN
 
 
-class KAN_CNN(nn.Module):
-    def __init__(self, layers_hidden, polynomial_order=2, base_activation=nn.ReLU):
-        super(KAN_CNN, self).__init__()
-
-        # quant de hidden layers
-        self.layers_hidden = layers_hidden
-        self.polynomial_order = polynomial_order
-        # funcao de ativaçao base pra ativar as camadas de input
-        self.base_activation = base_activation()
-
-        # Feature extractor with Convolutional layers
-
-        # Calculate the flattened feature size after convolutional layers
-        flat_features = 32 * 7 * 7
-        self.layers_hidden = [flat_features] + self.layers_hidden
-
-        self.base_weights = nn.ModuleList()
-        self.poly_weights = nn.ModuleList()
-        self.batch_norms = nn.ModuleList()
-
-        for in_features, out_features in zip(
-            self.layers_hidden[:-1], self.layers_hidden[1:]
-        ):
-            self.base_weights.append(nn.Linear(in_features, out_features))
-            self.poly_weights.append(
-                nn.Linear(in_features * (polynomial_order + 1), out_features)
-            )
-            self.batch_norms.append(nn.BatchNorm1d(out_features))
-
-    # funcao auxiliar pra computer monomios rapidamente(muito usado na KAN)
-    def compute_efficient_monomials(self, x, order):
-        powers = torch.arange(order + 1, device=x.device, dtype=x.dtype)
-        x_expanded = x.unsqueeze(-1).repeat(1, 1, order + 1)
-        return torch.pow(x_expanded, powers)
+class Conv_KAN(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = nn.Sequential(
+            ConvModule(),
+            KAN(width=[10, 10, 10], grid=6, k=5, symbolic_enabled=False))
 
     def forward(self, x):
-        # Reshape input from [batch_size, 784] to [batch_size, 1, 28, 28] for MNIST
-
-        # nao precisa pra minha rede
-        x = x.view(-1, 1, 28, 28)
-
-        x = self.feature_extractor(x)
-        x = x.view(x.size(0), -1)  # Flatten the features from the conv layers
-
-        for base_weight, poly_weight, batch_norm in zip(
-            self.base_weights, self.poly_weights, self.batch_norms
-        ):
-            base_output = base_weight(x)
-            monomial_basis = self.compute_efficient_monomials(
-                x, self.polynomial_order)
-            monomial_basis = monomial_basis.view(x.size(0), -1)
-            poly_output = poly_weight(monomial_basis)
-            x = self.base_activation(batch_norm(base_output + poly_output))
-
-        return x
+        return self.model(x)
 
 
 """ class BboxLoss(nn.Module):
